e4b1095facfb79b8592123153159b217e60dfc03
Emily Reynolds <emilyreynolds@fb.com>
2019-07-16 12:22:20 -0700

    Fact model

---
+++ app/src/main/java/com/codepath/fbu_newsfeed/Fact.java
@@ -0,0 +1,20 @@
+package com.codepath.fbu_newsfeed;
+
+public class Fact {
+
+    public TruthLevel truthLevel;
+
+    public Fact(TruthLevel truthLevel) { this.truthLevel = truthLevel; }
+
+    enum TruthLevel {
+        TRUE, M0STLY_TRUE, MIXTURE, MOSTLY_FALSE, FALSE, UNPROVEN;
+    }
+
+    public TruthLevel getTruthLevel() {
+        return truthLevel;
+    }
+
+    public void setTruthLevel(TruthLevel truthLevel) {
+        this.truthLevel = truthLevel;
+    }
+}
