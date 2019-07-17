fb2159bf847eda8e0036037104ba5c17c89d68e7
Emily Reynolds <emilyreynolds@fb.com>
2019-07-16 17:44:54 -0700

    functional sign up

--- app/src/main/java/com/codepath/fbu_newsfeed/SignUpActivity.java
+++ app/src/main/java/com/codepath/fbu_newsfeed/SignUpActivity.java
@@ -1,6 +1,8 @@
 package com.codepath.fbu_newsfeed;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,7 +10,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -41,6 +45,7 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     protected void setUser() {
+        String name = nameInput.getText().toString();
         String username = usernameInput.getText().toString();
         String password = passwordInput.getText().toString();
         String confirmPassword = confirmPasswordInput.getText().toString();
@@ -53,9 +58,25 @@ public class SignUpActivity extends AppCompatActivity {
 
         ParseUser user = new ParseUser();
 
+        user.put("fullName", name);
         user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
 
-
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.i("SignUp", "New user sign up successful");
+                    final Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Log.e("SignUp", "Error in new user sign up");
+                    Toast.makeText(SignUpActivity.this, "Error in sign up", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
 }
