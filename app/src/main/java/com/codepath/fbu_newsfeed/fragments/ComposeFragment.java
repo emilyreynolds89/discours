62ed05581ff5960d24b1ffa73f56ea7c055f649f
Emily Reynolds <emilyreynolds@fb.com>
2019-07-17 10:15:38 -0700

    ComposeFragment

---
+++ app/src/main/java/com/codepath/fbu_newsfeed/fragments/ComposeFragment.java
@@ -0,0 +1,16 @@
+package com.codepath.fbu_newsfeed.fragments;
+
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class ComposeFragment extends Fragment {
+
+    EditText etUrl;
+    ImageView ivArticlePreview;
+    TextView tvArticleTitle;
+
+
+}
