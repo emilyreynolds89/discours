62c67619d669cadb7b67c52e1f27d60acb77da40
Joshua Ibrahim <joshuaib@fb.com>
2019-07-18 16:34:22 -0700

    added sharing posts to Create

--- app/src/main/java/com/codepath/fbu_newsfeed/Fragments/CreateFragment.java
+++ app/src/main/java/com/codepath/fbu_newsfeed/Fragments/CreateFragment.java
@@ -20,11 +20,14 @@ import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.codepath.fbu_newsfeed.Models.Article;
+import com.codepath.fbu_newsfeed.Models.Share;
 import com.codepath.fbu_newsfeed.R;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +43,7 @@ public class CreateFragment extends Fragment {
     ImageButton ibReportCreate;
     EditText etCaptionCreate;
     Button btnShareCreate;
+    Article selectedArticle;
 
     ArrayAdapter<String> spinnerArrayAdapter;
 
@@ -82,11 +86,21 @@ public class CreateFragment extends Fragment {
                 if(imageFile != null) {
                     Glide.with(getContext()).load(imageFile.getUrl()).into(ivArticlePreviewCreate);
                 }
+                selectedArticle = articles.get(position);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
                 return;
+            }
+        });
+        btnShareCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (selectedArticle != null) {
+                    final String caption = etCaptionCreate.getText().toString();
+                    shareCreate(caption, selectedArticle);
+                }
             }
         });
 
@@ -116,6 +130,22 @@ public class CreateFragment extends Fragment {
                 }
             }
 
+        });
+    }
+    private void shareCreate(String caption, Article article) {
+        //final Share share = new Share(user, newArticle, caption);
+        Share share = new Share(ParseUser.getCurrentUser(), article, caption);
+        share.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("ComposeFragment", "Share article success");
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.flContainer, new FeedFragment()).commit();
+                } else {
+                    Log.e("ComposeFragment", "Error in sharing article");
+                    e.printStackTrace();
+                }
+            }
         });
     }
 }
