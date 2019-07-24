d446040c48d37d22651ce515ccacf3de9fdf5460
Emily Reynolds <emilyreynolds@fb.com>
2019-07-24 11:14:55 -0700

    login bug fixed

--- app/src/main/java/com/codepath/fbu_newsfeed/LoginActivity.java
+++ app/src/main/java/com/codepath/fbu_newsfeed/LoginActivity.java
@@ -15,6 +15,7 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
         ParseUser currentUser = ParseUser.getCurrentUser();
 
