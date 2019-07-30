44f9c9f20ba8970ad280a615616ea4178c8bf28f
Isabelle Zheng <isabellezheng@fb.com>
2019-07-29 17:40:47 -0700

    Click outside edittext to remove focus

--- app/src/main/java/com/codepath/fbu_newsfeed/HomeActivity.java
+++ app/src/main/java/com/codepath/fbu_newsfeed/HomeActivity.java
@@ -1,11 +1,17 @@
 package com.codepath.fbu_newsfeed;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -207,6 +213,23 @@ public class HomeActivity extends AppCompatActivity implements FragmentManager.O
                 Log.e(TAG, "onSharedIntent: nothing shared");
             }
         }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
     }
 
     public void onBackStackChanged() {
