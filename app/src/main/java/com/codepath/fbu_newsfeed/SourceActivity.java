3b148d4bf7a3155b9d1ce49f7fc727c9b49f89ad
Isabelle Zheng <isabellezheng@fb.com>
2019-08-01 21:35:05 -0700

    Add convenient scrolling and refreshing functionality to tags and sources pages

--- app/src/main/java/com/codepath/fbu_newsfeed/SourceActivity.java
+++ app/src/main/java/com/codepath/fbu_newsfeed/SourceActivity.java
@@ -1,12 +1,16 @@
 package com.codepath.fbu_newsfeed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,18 +18,25 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
 import com.codepath.fbu_newsfeed.Adapters.TrendsAdapter;
+import com.codepath.fbu_newsfeed.Helpers.EndlessRecyclerViewScrollListener;
 import com.codepath.fbu_newsfeed.Models.Article;
 import com.codepath.fbu_newsfeed.Models.Source;
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SourceActivity extends AppCompatActivity {
+
+    // TODO: NEED TO SHOW BIAS AND FACTUALNESS TOO
+    // TODO: MAYBE ALSO LINK TO WIKIPEDIA OR SOMETHING
+
     public static final String TAG = "SourceActivity";
     Source source;
 
@@ -33,9 +44,15 @@ public class SourceActivity extends AppCompatActivity {
     @BindView(R.id.tvSourceDescription) TextView tvSourceDescription;
     @BindView(R.id.ivSourceLogo) ImageView ivSourceLogo;
     @BindView(R.id.rvArticles) RecyclerView rvArticles;
+    @BindView(R.id.swipeContainer) SwipeRefreshLayout swipeContainer;
+
+    public @BindView(R.id.toolbar) Toolbar toolbar;
 
     ArrayList<Article> articles;
     TrendsAdapter adapter;
+
+    private EndlessRecyclerViewScrollListener scrollListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +68,7 @@ public class SourceActivity extends AppCompatActivity {
         articles = new ArrayList<>();
         adapter = new TrendsAdapter(this, articles);
 
+        rvArticles.setAdapter(adapter);
         final StaggeredGridLayoutManager gridLayoutManager = new StaggeredGridLayoutManager(2, RecyclerView.VERTICAL);
         rvArticles.setLayoutManager(gridLayoutManager);
 
@@ -60,9 +78,44 @@ public class SourceActivity extends AppCompatActivity {
         if (source.getLogo() != null)
             Glide.with(this).load(source.getLogo().getUrl()).into(ivSourceLogo);
 
-        // TODO: get stuff into recycler view
+        querySourceArticles(0);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchTimelineAsync();
+            }
+        });
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(R.color.colorAccentBold, R.color.colorAccentDark);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(gridLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                querySourceArticles(page);
+            }
+        };
+
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rvArticles.smoothScrollToPosition(0);
+            }
+        });
+
+        setSupportActionBar(toolbar);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        return true;
+    }
+
 
     private void querySource(String sourceId) {
         try {
@@ -72,6 +125,35 @@ public class SourceActivity extends AppCompatActivity {
             Log.d(TAG, "Error querying source", e);
         }
 
+    }
+
+    private void querySourceArticles(int offset) {
+        final ParseQuery<Article> articleQuery = new ParseQuery<Article>(Article.class);
+        articleQuery.include(Article.KEY_SOURCE);
+        articleQuery.whereEqualTo(Article.KEY_SOURCE, source);
+        articleQuery.setLimit(Article.LIMIT);
+        articleQuery.setSkip(offset * Article.LIMIT);
+        articleQuery.orderByDescending("createdAt");
+
+        articleQuery.findInBackground(new FindCallback<Article>() {
+            @Override
+            public void done(List<Article> newArticles, ParseException e) {
+                if (e != null) {
+                    Log.e("querySourceArticles", "Error with query");
+                    e.printStackTrace();
+                } else {
+                    adapter.addAll(newArticles);
+                }
+
+            }
+        });
+    }
+
+    private void fetchTimelineAsync() {
+        adapter.clear();
+        querySourceArticles(0);
+        swipeContainer.setRefreshing(false);
+        scrollListener.resetState();
     }
 
 }
--- app/src/main/java/com/codepath/fbu_newsfeed/TagActivity.java
+++ app/src/main/java/com/codepath/fbu_newsfeed/TagActivity.java
@@ -1,15 +1,20 @@
 package com.codepath.fbu_newsfeed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 import com.codepath.fbu_newsfeed.Adapters.TrendsAdapter;
+import com.codepath.fbu_newsfeed.Helpers.EndlessRecyclerViewScrollListener;
 import com.codepath.fbu_newsfeed.Models.Article;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -25,11 +30,17 @@ public class TagActivity extends AppCompatActivity {
 
     @BindView(R.id.tvTagHeader) TextView tvTagHeader;
     @BindView(R.id.rvArticles) RecyclerView rvArticles;
+    @BindView(R.id.swipeContainer) SwipeRefreshLayout swipeContainer;
+
+    public @BindView(R.id.toolbar) Toolbar toolbar;
 
     ArrayList<Article> articles;
     TrendsAdapter adapter;
 
+    EndlessRecyclerViewScrollListener scrollListener;
+
     String tag;
+
 
 
     @Override
@@ -52,7 +63,43 @@ public class TagActivity extends AppCompatActivity {
 
         queryArticles(0);
 
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchTimelineAsync();
+            }
+        });
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(R.color.colorAccentBold, R.color.colorAccentDark);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(gridLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                queryArticles(page);
+            }
+        };
+
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rvArticles.smoothScrollToPosition(0);
+            }
+        });
+
+
+        setSupportActionBar(toolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        return true;
+    }
+
 
     private void queryArticles(int offset) {
         final ParseQuery<Article> articleQuery = new ParseQuery<Article>(Article.class);
@@ -74,5 +121,12 @@ public class TagActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void fetchTimelineAsync() {
+        adapter.clear();
+        queryArticles(0);
+        swipeContainer.setRefreshing(false);
+        scrollListener.resetState();
     }
 }
