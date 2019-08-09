593ea9c4e9ee3ea36314d0d54674578b5ff85413
Emily Reynolds <emilyreynolds101@gmail.com>
2019-08-09 10:04:57 -0700

    link to quiz in user stats demo

--- app/src/main/java/com/codepath/fbu_newsfeed/Fragments/UserStatsDialogDemoFragment.java
+++ app/src/main/java/com/codepath/fbu_newsfeed/Fragments/UserStatsDialogDemoFragment.java
@@ -1,17 +1,28 @@
 package com.codepath.fbu_newsfeed.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.codepath.fbu_newsfeed.QuizActivity;
 import com.codepath.fbu_newsfeed.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class UserStatsDialogDemoFragment extends DialogFragment {
+
+    private Unbinder unbinder;
+
+    @BindView(R.id.tvClickHere) TextView tvClickHere;
 
     public UserStatsDialogDemoFragment() {}
 
@@ -22,7 +33,22 @@ public class UserStatsDialogDemoFragment extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user_stats_dialog_demo, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_stats_dialog_demo, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvClickHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), QuizActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
