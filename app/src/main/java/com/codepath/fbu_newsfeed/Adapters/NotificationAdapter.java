fdf295005cb29e71eda1d4609097a1c23ebd9d7c
Emily Reynolds <emilyreynolds101@gmail.com>
2019-08-09 14:30:48 -0700

    design

--- app/src/main/java/com/codepath/fbu_newsfeed/Adapters/NotificationAdapter.java
+++ app/src/main/java/com/codepath/fbu_newsfeed/Adapters/NotificationAdapter.java
@@ -196,7 +196,7 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
             }
         } catch (ParseException e) {
             e.printStackTrace();
-            return "Unfriended";
+            return "Friends";
         }
     }
 }
